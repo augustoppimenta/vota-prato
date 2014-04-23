e64a1d0f93d7d9f3c5bab8bd71ef9b9ef84da4ed
Augusto Pimenta <augustoppimenta@gmail.com>
2014-04-22 23:28:09 -0300

    Criando o model prato

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140423011927) do
+ActiveRecord::Schema.define(version: 20140423022730) do
 
   create_table "clientes", force: true do |t|
     t.string   "nome",       limit: 80
     t.integer  "idade"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pratos", force: true do |t|
+    t.string   "nome",       limit: 80
     t.datetime "created_at"
     t.datetime "updated_at"
   end
