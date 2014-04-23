b1e38bdf78cfead0a1eb6feabb6f4cb7156066f3
Augusto Pimenta <augustoppimenta@gmail.com>
2014-04-23 20:43:09 -0300

    Adicionando novas validações no models prato, qualificação e receita

--- app/models/receita.rb
+++ app/models/receita.rb
@@ -2,4 +2,7 @@ class Receita < ActiveRecord::Base
   validates_presence_of :conteudo, message: " - deve ser preenchido"
 
   belongs_to :prato
+
+  validates_presence_of :prato_id
+  validates_associated :prato
 end
