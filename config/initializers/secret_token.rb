d2f9aa111976244138886d3162b1d6fa697bd95f
Augusto Pimenta <augustoppimenta@gmail.com>
2014-04-22 21:54:31 -0300

    Iniciando o projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VotaPrato::Application.config.secret_key_base = '4dc4c08cfe9efacb9cb16a9bc726e4833ee2177bef2ac6cdd77bd0c980e3b9944219177690c82a5088ce397932c5fca5a2f598a73a289abfe6b862625b93a0c9'
