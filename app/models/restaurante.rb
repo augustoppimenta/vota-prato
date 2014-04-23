21b81880a93b3aba279eba425fa201b06a249c28
Augusto Pimenta <augustoppimenta@gmail.com>
2014-04-23 20:10:14 -0300

    Adicionadno os relacionamentos nos models

--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -7,6 +7,11 @@ class Restaurante < ActiveRecord::Base
   validates_uniqueness_of :endereco, message: "endereço já cadastrado"
 
   validate :primeira_letra_deve_ser_maiuscula
+
+  has_many :qualificacoes
+  has_and_belongs_to_many :pratos
+
+
   private
     def primeira_letra_deve_ser_maiuscula
       errors.add(:nome, "primeira letra deve ser maiúscula") unless nome =~ /[A-Z].*/
