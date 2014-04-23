35ca342360c8c1c412b9599206205a67a92f38ca
Augusto Pimenta <augustoppimenta@gmail.com>
2014-04-23 20:33:46 -0300

    Criando nova tabela de relacionamento entre Pratos e Restaurantes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140423232222) do
+ActiveRecord::Schema.define(version: 20140423233014) do
 
   create_table "clientes", force: true do |t|
     t.string   "nome",       limit: 80
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20140423232222) do
     t.string   "nome",       limit: 80
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "pratos_restaurantes", id: false, force: true do |t|
+    t.integer "prato_id"
+    t.integer "restaurante_id"
   end
 
   create_table "qualificacoes", force: true do |t|
