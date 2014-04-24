df4e3c1e251c6076e8a2fbb85f9fbbc15bf001d9
Augusto Pimenta <augustoppimenta@gmail.com>
2014-04-24 13:30:16 -0300

    Disponibilizando nas views o form comentario

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
       separator: ",",
       delimiter: "."
   end
+
+  def novo_comentario(comentavel)
+    render partial: "comentarios/novo_comentario", locals: {comentavel: comentavel}
+  end
 end
