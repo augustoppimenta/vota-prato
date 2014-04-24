4bc76d2cd2d79bc8eaebb9e8a230a8bf7e6c59d2
Augusto Pimenta <augustoppimenta@gmail.com>
2014-04-24 00:33:37 -0300

    criando nova action Create

--- app/controllers/restaurantes_controller.rb
+++ app/controllers/restaurantes_controller.rb
@@ -18,4 +18,16 @@ class RestaurantesController < ApplicationController
   def new
     @restaurante = Restaurante.new
   end
+
+  def create
+    @restaurante = Restaurante.new(restaurante_params)
+    @restaurante.save
+    redirect_to action: 'show', id: @restaurante
+  end
+
+  private
+
+    def restaurante_params
+      params.require(:restaurante).permit(:nome, :endereco, :especialidade)
+    end
 end
